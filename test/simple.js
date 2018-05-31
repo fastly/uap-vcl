29d04b38bead8e8e0beb2d56d6cc5bcbd560b1df
Leon Brocard <lbrocard@fastly.com>
2018-05-31 11:25:10 +0100

    Pretty the test

--- test/simple.js
+++ test/simple.js
@@ -8,20 +8,23 @@ chai.use(chaiHttp);
 const uri = process.env.URI || 'https://uap-vcl.global.ssl.fastly.net';
 
 it('receives an HTML response from /', () =>
-  chai.request(uri)
+  chai
+    .request(uri)
     .get('/')
     .then(res => {
       expect(res).to.have.status(200);
       expect(res).to.be.html;
       expect(res.text).to.include('ua-parser in VCL');
-    })
-);
+    }));
 
 try {
   const doc = yaml.safeLoad(fs.readFileSync('test_ua.yaml', 'utf8'));
-  doc['test_cases'].forEach((test) => {
-    it(`receives a response for ${test.user_agent_string} to be ${test.family} / ${test.major}`, () =>
-      chai.request(uri)
+  doc['test_cases'].forEach(test => {
+    it(`receives a response for ${test.user_agent_string} to be ${
+      test.family
+    } / ${test.major}`, () =>
+      chai
+        .request(uri)
         .get('/')
         .set('User-Agent', test.user_agent_string)
         .then(res => {
@@ -29,12 +32,19 @@ try {
           expect(res).to.have.status(200);
           expect(res).to.be.html;
           expect(res.text).to.include('ua-parser in VCL');
-          expect(res.text).to.include(`<th>ua_family</th><td>${test.family}</td>`);
-          expect(res.text).to.include(`<th>ua_major</th><td>${test.major}</td>`);
-          expect(res.text).to.include(`<th>ua_minor</th><td>${test.minor}</td>`);
-          expect(res.text).to.include(`<th>ua_patch</th><td>${test.patch}</td>`);
-        })
-    ).timeout(5000);
+          expect(res.text).to.include(
+            `<th>ua_family</th><td>${test.family}</td>`
+          );
+          expect(res.text).to.include(
+            `<th>ua_major</th><td>${test.major}</td>`
+          );
+          expect(res.text).to.include(
+            `<th>ua_minor</th><td>${test.minor}</td>`
+          );
+          expect(res.text).to.include(
+            `<th>ua_patch</th><td>${test.patch}</td>`
+          );
+        })).timeout(5000);
   });
 } catch (e) {
   console.log(e);
